d27185c070c4826771f7eab36e2af55161a78501
parthbhutada <parth.bhutada.btech2023@sitpune.edu.in>
2025-01-22 21:04:34 +0530

    added subtraction function

--- Calculator.java
+++ Calculator.java
@@ -2,4 +2,7 @@ public class Calculator {
     int addition(int num1, int num2) {
         return num1 + num2;
     }
+    int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
 }
