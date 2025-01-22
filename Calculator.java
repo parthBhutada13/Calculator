5460019d89a5bd1f790b893dc1a66e2b47ff4159
parthbhutada <parth.bhutada.btech2023@sitpune.edu.in>
2025-01-22 21:05:53 +0530

    added division function

--- Calculator.java
+++ Calculator.java
@@ -8,4 +8,12 @@ public class Calculator {
     int multiplication(int num1, int num2) {
         return num1 * num2;
     }
+    float division(int num1, int num2) {
+        if (num2 == 0) {
+            System.out.println("Division by zero is invalid!");
+            return 0.0f;
+        } else {
+            return (float) num1 / num2;
+        }
+    }
 }
