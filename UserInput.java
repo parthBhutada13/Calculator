56ae1a99624043d145c8930ff22e6d5a394a0f99
parthbhutada <parth.bhutada.btech2023@sitpune.edu.in>
2025-01-22 21:12:31 +0530

    added input function

--- UserInput.java
+++ UserInput.java
@@ -1,2 +1,15 @@
+import java.util.*;
 public class UserInput {
+    int[] userInput() {
+        int[] numbers = new int[2];
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        numbers[0] = scan.nextInt();
+
+        System.out.println("Enter the second number:");
+        numbers[1] = scan.nextInt();
+
+        return numbers;
+    }
 }
