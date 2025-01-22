3fb056c9bb68b918e0f5dc113c51687cd70a5272
parthbhutada <parth.bhutada.btech2023@sitpune.edu.in>
2025-01-22 21:14:08 +0530

    added main class

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Main.java
+//Name:- Parth Bhutada
+//PRN:- 23070126084
+//Batch:- B1
+
+public class Main {
+    public static void main(String[] args) {
+        UserInput userInput = new UserInput();
+        userInput.performOperation();
+    }
+}
